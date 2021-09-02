af3f2c556724231c49e50009c78f26b9ebb2f2d5
hansyao <hansyow@gmail.com>
2021-09-02 18:40:51 +0800

    add network restart to kill TIME_WAIT connections

--- Actions/conn_test_by_clash_api.sh
+++ Actions/conn_test_by_clash_api.sh
@@ -182,7 +182,7 @@ function connetion_test() {
     local CONTROLLER=${CONTROLLER}
 
     echo "开始规则转换，并启动clash客户端"
-    sudo /etc/init.d/networking restart
+    sudo /etc/init.d/network-manager restart
     start_clash ${POOL}
 	echo "暂停 2 秒 确保clash启动正常"
     sleep 2
