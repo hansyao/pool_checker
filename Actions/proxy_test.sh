2132369e82392ccc4427336c609c6d17692008c7
hansyao <hansyow@gmail.com>
2021-09-03 00:57:43 +0800

    deploy redsockets

--- Actions/proxy_test.sh
+++ Actions/proxy_test.sh
@@ -28,9 +28,11 @@ firwall_set() {
 	sudo iptables -t nat -F REDSOCKS
 	sudo iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+	sudo iptables -t nat -A REDSOCKS -d 10.1.0.0/8 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
+	sudo iptables -t nat -A REDSOCKS -d 172.17.0.0/12 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
@@ -161,7 +163,7 @@ redsocks {
 redudp {
 	local_ip = 127.0.0.1;
 	local_port = 8888;
-	ip = 0.0.0.0;
+	ip = 127.0.0.1;
 	port = 7891;
 }
 
