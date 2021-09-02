46ca6589888a815a959ca05efa4bad7e4f12c819
hansyao <hansyow@gmail.com>
2021-09-03 02:16:38 +0800

    deploy redsockets

--- Actions/proxy_test.sh
+++ Actions/proxy_test.sh
@@ -27,11 +27,14 @@ firwall_set() {
 	# 忽略局域网地址
 	sudo iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+	sudo iptables -t nat -A REDSOCKS -d 10.1.0.0/8 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
 	sudo iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
+	sudo iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
+
 	sudo iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
 
 	# 把流量转发到 12345 端口，即redsocks
