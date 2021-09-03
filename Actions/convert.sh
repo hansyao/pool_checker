01e9cf83e30cc58e82fb0dd404e5b1c1d0f16199
hansyao <hansyow@gmail.com>
2021-09-03 17:42:49 +0800

    update subconverter

--- Actions/convert.sh
+++ Actions/convert.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
 
 TEMP_DIR=/tmp
-GITHUB=http/github.com
-OS=linux64
-USER=tindy2013
-APP=subconverter
-REPO=$USER/$APP
-FILE=${TEMP_DIR}/$APP.tar.gz
-PROXY_URL=https://lingering-math-d2ca.hansyow.workers.dev/
 
-get_latest_release() {
-  curl --silent "${PROXY_URL}https/api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
-}
-VERSION=$(get_latest_release $REPO)
-
-rm -rf $APP
-rm -f $FILE
-
-echo -e "下载$FILE"
-curl -s -L -X GET ${PROXY_URL}${GITHUB}/${REPO}/releases/download/$VERSION/$APP\_$OS.tar.gz -o $FILE
-echo -e "下载完成, 开始解压缩到 ${TEMP_DIR}/$APP"
-tar -xvf $FILE -C ${TEMP_DIR}/ >/dev/null 2>&1
-echo -e "解压完成，删除 $FILE"
-rm -f $FILE
-
-echo -e "设置权限 chmod 755 ${TEMP_DIR}/$APP/$APP"
-chmod 755 ${TEMP_DIR}/$APP/$APP
-echo -e "扩充EMOJI库"
-cp -f ./Actions/emoji.txt ${TEMP_DIR}/${APP}/snippets/emoji.txt
-echo -e "更改缺省参数"
-sed -i "s/log_level\=.*/log_level\=silent/g" ${TEMP_DIR}/${APP}/pref.ini
+cp -rf subconverter ${TEMP_DIR}
 echo -e "运行规则转换工具"
-${TEMP_DIR}/$APP/$APP >/dev/null 2>&1 &
+${TEMP_DIR}/subconverter/subconverter >/dev/null 2>&1 &
