4a74dd46ca2631ea53061ab2cb20a2d9225b15dc
hansyao <hansyow@gmail.com>
2021-09-09 15:51:05 +0800

    customized configs for subconverter

--- env.sh
+++ env.sh
@@ -49,11 +49,15 @@ get_clash
 if [[ $[PLATFORM] -eq 1 ]]; then
 	cp -rf /opt/share/git-core /tmp/
 	cp -rf /opt/subconverter /tmp/
+	cp -f `pwd`/Actions/emoji.txt /tmp/subconverter/snippets/
+	cf -f `pwd`/Actions/pref.ini /tmp/subconverter/
 	exit 0
 fi
 
 cp -rf `pwd`/share/git-core /tmp/
 cp -rf `pwd`/subconverter /tmp/
+cp -f `pwd`/Actions/emoji.txt /tmp/subconverter/snippets/
+cf -f `pwd`/Actions/pref.ini /tmp/subconverter/
 
 sudo bash -c 'echo 10 >/proc/sys/net/ipv4/tcp_fin_timeout'
 sudo bash -c 'echo 10 >/proc/sys/net/ipv4/tcp_keepalive_time'
