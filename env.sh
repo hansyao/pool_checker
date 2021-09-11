f4bb1da6152e1545b4b942d210e84cde12786ea4
hansyao <hansyow@gmail.com>
2021-09-11 21:59:40 +0800

    fix pwd path

--- env.sh
+++ env.sh
@@ -60,7 +60,7 @@ cp -rf `pwd`/share/git-core /tmp/
 cp -rf `pwd`/subconverter /tmp/
 cp -f `pwd`/Actions/emoji.txt /tmp/subconverter/snippets/
 cp -f `pwd`/Actions/pref.ini /tmp/subconverter/
-cp -f 'pwd'/Actions/all_base.tpl /tmp/subconverter/base/all_base.tpl
+cp -f `pwd`/Actions/all_base.tpl /tmp/subconverter/base/all_base.tpl
 
 sudo bash -c 'echo 10 >/proc/sys/net/ipv4/tcp_fin_timeout'
 sudo bash -c 'echo 10 >/proc/sys/net/ipv4/tcp_keepalive_time'
